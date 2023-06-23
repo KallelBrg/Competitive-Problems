801585112b43adf80b96aef74f33a9326ccfea26
Kallel Braga Bezerra <107359212+KallelBrg@users.noreply.github.com>
2023-06-23 15:06:39 -0300

    Create template.cpp

---
+++ AlgoritmosMaratona/template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define f first
+#define s second
+#define tos to_string
+#define inf 1000000000
+#define entrada freopen("input.txt", "r", stdin);
+#define saida freopen("output.txt", "w", stdout);
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
